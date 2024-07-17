a35f8688c3fe404d3b23697cdfe893e505f8f6f4
Brandon Restrepo <jhon2.restrepo@ucp.edu.co>
2024-07-17 16:02:15 -0500

    ...

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
      require_once "./config/app.php";
      require_once "./autoload.php";
-     require_once "./app/views/inc/session_start.php";
      if(isset($_GET['views'])){
         $url = explode("/", $_GET['views']);
     }else{
